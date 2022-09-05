9054aad326782a96296f015520ef39bb925172a6
Oonie11 <utkarshpatel002@gmail.com>
2022-09-05 19:16:20 -0400

    change title name

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import Card from './Card.js';
+import React from "react";
+import Card from "./Card.js";
 
 // we are writing robots in {} in parameter section because we are destructuring props. Ex:- {robots} = props; instead of writing this whole expression we are directly writing {robots} as argument.
-const CardList = ({robots}) => {
-    
+const CardList = ({ robots }) => {
+  return robots.map((each) => {
     return (
-         robots.map((each)=>{
-            return <Card key={each.id} id={each.id} name={each.name} email={each.email}/>
-        })
-    )
+      <Card key={each.id} id={each.id} name={each.name} email={each.email} />
+    );
+  });
 };
 
 export default CardList;
