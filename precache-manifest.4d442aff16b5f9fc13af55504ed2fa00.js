7e5237a4784b6201b754394d57e89a76b5305eea
Oonie11 <utkarshpatel002@gmail.com>
2022-09-05 18:36:33 -0400

    Updates

---
+++ precache-manifest.4d442aff16b5f9fc13af55504ed2fa00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ead1d6fcc072368caf6",
+    "url": "/robo-friends/static/css/main.f7ba6e87.chunk.css"
+  },
+  {
+    "revision": "7ead1d6fcc072368caf6",
+    "url": "/robo-friends/static/js/main.7ead1d6f.chunk.js"
+  },
+  {
+    "revision": "c34a39ba9cc453e022f2",
+    "url": "/robo-friends/static/css/1.751c1303.chunk.css"
+  },
+  {
+    "revision": "c34a39ba9cc453e022f2",
+    "url": "/robo-friends/static/js/1.c34a39ba.chunk.js"
+  },
+  {
+    "revision": "617e3c50e67378d4af10",
+    "url": "/robo-friends/static/js/runtime~main.617e3c50.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo-friends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "80e266a557a11a2f13993d65010716dc",
+    "url": "/robo-friends/index.html"
+  }
+];
